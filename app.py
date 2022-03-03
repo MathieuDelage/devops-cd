af7b05862bc5497939e6f5cb74087d582a84f115
Mathieu Delage <delage.mathieu.pro@gmail.com>
2022-03-03 16:54:00 +0100

    docs: documentation du fichier app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+"""
+Projet de démo pour la création d'un CD
+"""
 from flask import Flask
 import os
 
+# Récupération des ENVs
 PORT = os.environ.get("PORT", 80)
 HOST = os.environ.get("HOST", "0.0.0.0")
 
@@ -8,6 +12,11 @@ app = Flask("demo")
 
 @app.get("/")
 def hello():
+    """Fonction appellée pour le chemin par défaut.
+
+    Returns:
+        str: un message de la plus grande importance
+    """
     return "Hello, World !"
 
 if __name__ == "__main__":
